0a894cb7262522abe3f14afb027e4420d8824ee4
Bohdan <qwebeck5@gmail.com>
2021-09-16 08:04:00 +0200

    Added amount to config

--- preprocessing_pipeline/preprocessing_links/transform_nifti_to_npy.py
+++ preprocessing_pipeline/preprocessing_links/transform_nifti_to_npy.py
@@ -9,7 +9,11 @@ class TransformNiftiToNpy(ChainLink):
     def run(self, global_config: Dict[str, str]):
         link_config = global_config.get('transform_nifti_to_npy', None)
         if self.is_activated(link_config):
+            amount = 0
             for operation in link_config["operations"]:
+                if amount == link_config["amount"]:
+                    break
+                amount += 1
                 self._transform_nifti_to_npy(
                     operation['path_from'], operation['path_to'])
 
@@ -17,8 +21,8 @@ class TransformNiftiToNpy(ChainLink):
         for nifti_file_name in os.listdir(path_from):
             nifti_path = os.path.join(path_from, nifti_file_name)
             if not os.path.isdir(path_to):
-                os.makedirs(path_to)   
-        
+                os.makedirs(path_to)
+
             if 'nii' in nifti_path:
                 nifti = nib.load(nifti_path)
                 npy = nifti.get_fdata()
