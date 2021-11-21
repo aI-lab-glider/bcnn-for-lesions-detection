50b420b271177cc5cfe6c86e93dfd54231994250
alicjak1519 <akrzeminska1519@gmail.com>
2021-11-21 01:01:50 +0100

    Remove old and unneeded files

--- network/toy_train_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rm -rf toy_data toy_weights toy_images toy_predict
-python generate_toy_data.py
-python train.py with configs/toy_config.json
-python train.py with configs/toy_config.json "bayesian"=False
-python test.py with configs/toy_config.json
-python test.py with configs/toy_config.json "bayesian"=False
-
