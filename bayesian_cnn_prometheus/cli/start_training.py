4ad271a203f2b5e393c24dd68dc3f79082d7c9d2
Bohdan <qwebeck5@gmail.com>
2021-11-27 14:35:15 +0100

    Fixes JON

--- bayesian_cnn_prometheus/cli/start_training.py
+++ bayesian_cnn_prometheus/cli/start_training.py
@@ -6,5 +6,5 @@ from bayesian_cnn_prometheus.constants import Paths
 
 @click.command()
 def start_training():
-    print('Jon started starting submit a traing job ...')
+    print('Jon strated submiting a train job ...')
     os.system(f'sbatch run_python_script.sh {Paths.PROJECT_DIR/"main.py"}')
